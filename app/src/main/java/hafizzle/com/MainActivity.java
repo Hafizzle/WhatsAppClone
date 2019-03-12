63d149ee0774f75803ca8c55fe8172e97f533b13
Hafizzle <34069775+Hafizzle@users.noreply.github.com>
2019-03-12 11:03:52 -0500

    Add phone login/verification
    In progress

--- app/src/main/java/hafizzle/com/MainActivity.java
+++ app/src/main/java/hafizzle/com/MainActivity.java
@@ -2,12 +2,21 @@ package hafizzle.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mPhoneNumber, mCode;
+    private Button mSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPhoneNumber = findViewById(R.id.phoneNumber);
+        mCode = findViewById(R.id.code);
+        mSend = findViewById(R.id.send);
     }
 }
